9709a4e47254e3bdc572fb570f3f2a0f84e44fed
Abishazaigo <you@example.com>
2021-01-29 13:08:20 +0530

    Card alignment is updated

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -5,29 +5,23 @@ const Posts = (props) => {
       {props.posts.map((post) => {
         return (
             <div className="row">
-              <div className="col-2">
+              <div className="col-5">
               </div>
-              <div className="col-8">
-                <div className="card" style={{ width: "300px", height:"300px"}}>
-                  <h6 className="card-header" style={{ color: "#59a310" }}>{post.title}</h6>
+              <div className="col-6">
+                <div className="card mb-5" style={{ width: "300px", height:"300px"}}>
+                  <h6 className="card-header h-25" style={{ color: "#59a310" }}>{post.title}</h6>
                   <p className="card-body" style={{ textAlign: "justify" }}>{post.body}</p>
-                  
                     <div className="row">
-                      <div className="col-1"></div>
-                      <div className=" card-footer col-5 text-left">
-                      
-                    <Link to="/View">View More</Link>
-                    
-                    </div>
-                    <div className=" card-footer col-5 text-right">
-                    
-                    <Link to="/Comment">Comment</Link>
-                    <div className="col-1"></div>
-                    </div>
+                      <div className="text-left p-4">
+                        <Link to="/View">View More</Link>
+                      </div>
+                      <div className="text-right p-4">
+                        <Link to="/Comment">Comment</Link>
+                      </div>
                     </div>
                 </div>
-                <div className="col-2">
-                </div>
+              </div>
+              <div className="col-1">
               </div>
             </div>
         );
